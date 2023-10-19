daf33545427fe03816a08ac9388f1ca2969f2121
ssandrini <ssandrini@itba.edu.ar>
2023-10-19 15:57:41 -0300

    fix

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,18 +2,18 @@ module "vpc" {
   source = "./modules/vpc"
 }
 
-#module "lambda" {
-#  source     = "./modules/lambda"
-#  subnet_ids = module.vpc.subnet_ids
-#  account_id = data.aws_caller_identity.this.account_id
-#  vpc_id     = module.vpc.main_vpc_id
-#  api_gw_execution_arn = module.api-gw.execution_arn
-#}
-#
-#module "api-gw" {
-#  source = "./modules/api-gw"
-#  lambda_fun = module.lambda.invoke_arn
-#}
+module "lambda" {
+  source     = "./modules/lambda"
+  subnet_ids = module.vpc.subnet_ids
+  account_id = data.aws_caller_identity.this.account_id
+  vpc_id     = module.vpc.main_vpc_id
+  api_gw_execution_arn = module.api-gw.execution_arn
+}
+
+module "api-gw" {
+  source = "./modules/api-gw"
+  lambda_fun = module.lambda.invoke_arn
+}
 
 module "S3" {
     source = "./modules/S3"

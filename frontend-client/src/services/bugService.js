240251a9e82a16cce5871386d6debdc051e3c6b2
gbeade <gbeade@itba.edu.ar>
2023-12-16 16:29:02 -0300

    Fix board moving cards

--- frontend-client/src/services/bugService.js
+++ frontend-client/src/services/bugService.js
@@ -5,9 +5,9 @@ import { API_URL } from './config';
 async function getBugsByBoardId(boardId) {
   // Mock data, replace with actual fetch logic
   const mockData = [
-    { id: 'bug1', title: 'Bug 1', description: 'Description for Bug 1', label: '30 mins' },
-    { id: 'bug2', title: 'Bug 2', description: 'Description for Bug 2', label: '45 mins' },
-    { id: 'bug2', title: 'Arreglar kanban board', description: 'Está todo roto', label: '45 mins' },
+    { id: 'bug1', title: 'Bug 1', description: 'Description for Bug 1', label: '30 mins', progress: "ICEBOX" },
+    { id: 'bug2', title: 'Bug 2', description: 'Description for Bug 2', label: '45 mins', progress: "TODO" },
+    { id: 'bug98', title: 'Arreglar kanban board', description: 'Está todo roto', label: '45 mins', progress: "TODO"},
     // Add more bugs as needed
   ];
 
@@ -39,4 +39,10 @@ async function getBugById(bugId) {
   return bug;
 }
 
-export { getBugsByBoardId, getBugById };
+
+function changeBugProgress(bugId, newProgress) {
+  console.log(`The bug ${bugId} changed its progress to ${newProgress}`)
+}
+
+
+export { getBugsByBoardId, getBugById, changeBugProgress};
--- frontend-client/src/views/BoardView/BoardView.js
+++ frontend-client/src/views/BoardView/BoardView.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { useParams, useNavigate } from 'react-router-dom';
 import Board from 'react-ui-kanban';
 import { Divider } from 'antd';
-import { getBugsByBoardId} from '../../services/bugService';
+import { getBugsByBoardId, changeBugProgress} from '../../services/bugService';
 import { getBoardById} from '../../services/boardService';
 
 
@@ -39,33 +39,92 @@ const BoardDetailsCard = () => {
   );
 };
 
+function bugCollectionToLaneSchema(bugs) {
+  // Create initial data structure
+  const kanbanData = {
+    lanes: [
+      {
+        id: "ICEBOX",
+        title: 'Icebox',
+        label: '0/0',
+        cards: []
+      },
+      {
+        id: "TODO",
+        title: 'To Do',
+        label: '0/0',
+        cards: []
+      },
+      {
+        id: "DOING",
+        title: 'Doing',
+        label: '0/0',
+        cards: []
+      },
+      {
+        id: "DONE",
+        title: 'Done',
+        label: '0/0',
+        cards: []
+      }
+    ]
+  };
+
+  // Iterate through bugs and add them to the appropriate lane based on progress
+  bugs.forEach((bug, index) => {
+    const card = {
+      id: bug.id,
+      title: bug.title,
+      description: bug.description,
+      label: bug.label || '',
+      draggable: true,
+      metadata: {
+        bugId: bug.bugId,
+        progress: bug.progress
+      }
+    };
+
+    // Determine the lane index based on the progress
+    let laneIndex;
+    switch (bug.progress) {
+      case 'ICEBOX':
+        laneIndex = 0;
+        break;
+      case 'TODO':
+        laneIndex = 1;
+        break;
+      case 'DOING':
+        laneIndex = 2;
+        break;
+      case 'DONE':
+        laneIndex = 3;
+        break;
+      default:
+        // Set a default lane (you can customize this based on your needs)
+        laneIndex = 0;
+    }
+
+    // Add the card to the appropriate lane
+    kanbanData.lanes[laneIndex].cards.push(card);
+  });
+
+  // Update the labels for each lane
+  kanbanData.lanes.forEach((lane, index) => {
+    lane.label = `${kanbanData.lanes[index].cards.length}/${kanbanData.lanes[index].cards.length}`;
+  });
+
+  return kanbanData;
+}
+
+
 function BoardView() {
   const navigate = useNavigate();
   const { boardId } = useParams();
 
   // State for holding bug data
-  const [boardData, setBoardData] = useState({
-    lanes: [
-      {
-        id: 'lane1',
-        title: 'To Do',
-        label: '2/2',
-        cards: [],
-      },
-      {
-        id: 'lane2',
-        title: 'In Progress',
-        label: '0/0',
-        cards: [],
-      },
-      {
-        id: 'lane3',
-        title: 'Done',
-        label: '0/0',
-        cards: [],
-      },
-    ],
-  });
+  const [boardData, setBoardData] = useState(bugCollectionToLaneSchema([]))
+  const [bugs, setBugs] = useState(bugCollectionToLaneSchema([]))
+
 
   useEffect(() => {
     // Fetch bug data when the component mounts
@@ -76,19 +135,12 @@ function BoardView() {
         description: bug.description,
         label: bug.label,
         bugId: bug.id,
+        progress: bug.progress
       }));
 
       // Set the bug cards to the 'To Do' lane
-      setBoardData((prevData) => ({
-        ...prevData,
-        lanes: [
-          {
-            ...prevData.lanes[0],
-            cards: bugCards,
-          },
-          ...prevData.lanes.slice(1),
-        ],
-      }));
+      setBugs(bugCards)
+      setBoardData(bugCollectionToLaneSchema(bugCards));
     });
   }, [boardId]);
 
@@ -96,9 +148,14 @@ function BoardView() {
     <div style={{ padding: '20px', minHeight: '100vh', background: '#3179ba' }}>
       <BoardDetailsCard /> {/* Render the details card */}
       <Board
+      onCardMoveAcrossLanes = {(fromLaneId, toLaneId, bugId, index) => {
+        console.log(fromLaneId, toLaneId, bugId, index); 
+        changeBugProgress(bugId, toLaneId)
+      }}
+      onCardDelete={(cardId, laneId) => {console.log("POOF", cardId)}}
         onCardClick={(cardId, metadata, laneId) => {
           // Redirect to /boards/:boardId/bugs/:bugId when a card is clicked
-          const bugId = boardData.lanes.find((lane) => lane.id === laneId)?.cards.find((card) => card.id === cardId)?.bugId;
+          const bugId = bugs.find((bug) => bug.id === cardId)?.bugId;
           if (bugId) {
             navigate(`/boards/${boardId}/bugs/${bugId}`);
           }

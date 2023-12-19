df3ffb4711a7ae9718b1bf5425695e3716e8d6ac
Uri <umihura@itba.edu.ar>
2023-12-18 21:54:59 -0300

    update bug lambda

--- terraform/resources/lambda/src/updateBug.js
+++ terraform/resources/lambda/src/updateBug.js
@@ -25,7 +25,31 @@ exports.handler = async (event, context) => {
         return response;
     }
 
-    const { description, due_by, stage, bug_id } = bugData;
+    let bug_id;
+    let board_id;
+    const pathMatch = event.path.match(/\/boards\/\d+\/bugs\/(\d+)/);
+
+    if (pathMatch && pathMatch[1]) {
+        // Extract bug_id from the matched groups
+        bug_id = parseInt(pathMatch[1], 10);
+        board_id = parseInt(pathMatch[1], 10);
+    } else {
+        // Handle the case where the URL doesn't match the expected pattern
+        response.statusCode = 400;
+        response.body = 'Invalid URL format';
+        return response;
+    }
+    
+    console.log("asdasdasd");
+    console.log(bug_id);
+
+    if(bugData == undefined){
+        response.statusCode = 400; // Bad Request
+        response.body = 'Body must contain data';
+        return response;
+    }
+
+    const { description, due_by, stage } = bugData;
 
     if (!['icebox', 'to-do', 'doing', 'done'].includes(stage)) {
         console.error('Invalid stage:', stage);
@@ -34,31 +58,54 @@ exports.handler = async (event, context) => {
         return response;
     }
 
-    let updateBugQuery = {
-        text: 'UPDATE bugs SET',
-        values: [],
-    };
+    let updateBugQuery1 = undefined;
+    let updateBugQuery2 = undefined;
+    let updateBugQuery3 = undefined;
 
-    const setClause = [];
     if (description !== undefined) {
-        setClause.push(`description = $${updateBugQuery.values.push(description)}`);
+        updateBugQuery1 = {
+            text: `
+                UPDATE bugs
+                SET
+                    description = $1
+                WHERE
+                    bug_id = $2    
+            `,
+            values: [description, bug_id],
+        };
     }
     if (due_by !== undefined) {
-        setClause.push(`due_by = $${updateBugQuery.values.push(due_by)}`);
+        updateBugQuery2 = {
+            text: `
+                UPDATE bugs
+                SET
+                    due_by = $1
+                WHERE
+                    bug_id = $2    
+            `,
+            values: [due_by, bug_id],
+        };
     }
     if (stage !== undefined) {
-        setClause.push(`stage = $${updateBugQuery.values.push(stage)}`);
+        updateBugQuery3 = {
+            text: `
+                UPDATE bugs
+                SET
+                    stage = $1
+                WHERE
+                    bug_id = $2    
+            `,
+            values: [stage, bug_id],
+        };
     }
-
-    if (setClause.length === 0) {
-        response.statusCode = 400;
-        response.body = 'No valid fields provided for update';
-        return response;
-    }
-
-    updateBugQuery.text += ` ${setClause.join(', ')}`;
-    updateBugQuery.text += ' WHERE bug_id = $1';
-    updateBugQuery.values.unshift(bug_id);
+    console.log("1111111");
+    console.log(description);
+    console.log(due_by);
+    console.log(stage);
+    
+    console.log(updateBugQuery1);
+    console.log(updateBugQuery2);
+    console.log(updateBugQuery3);
 
     // end query:
     // const updateBugQuery = {
@@ -69,8 +116,7 @@ exports.handler = async (event, context) => {
     //             due_by = $2,
     //             stage = $3
     //         WHERE
-    //             bug_id = $4;
-            
+    //             bug_id = $4;    
     //     `,
     //     values: [description, due_by, stage, bug_id],
     // };
@@ -84,14 +130,27 @@ exports.handler = async (event, context) => {
         console.log('Connected to the database.');
 
         console.log('Executing the updateBugQuery query...');
-        await client.query(updateBugQuery);
+        if(updateBugQuery1 != undefined) {
+            await client.query(updateBugQuery1);
+            console.log('updateBugQuery1 query executed successfully.');
+        }
+        if(updateBugQuery2 != undefined) {
+            await client.query(updateBugQuery2);
+            console.log('updateBugQuery2 query executed successfully.');
+        }
+        if(updateBugQuery3 != undefined) {
+            await client.query(updateBugQuery3);
+            console.log('updateBugQuery3 query executed successfully.');
+        }
+
+        // await client.query(updateBugQuery);
         console.log('updateBugQuery query executed successfully.');
 
         console.log('Closing the database connection...');
         await client.end();
         console.log('Database connection closed.');
 
-        response.body = 'Bug created';
+        response.body = 'Bug updated';
     } catch (error) {
         console.error('Error updating bug:', error);
         response.statusCode = 500;
@@ -105,7 +164,7 @@ exports.handler = async (event, context) => {
     try {
         const message = {
             TopicArn: process.env.SNS_HOST,
-            Message: 'Updated bug ' + bug.bug_id + ' from your board!',
+            Message: 'There was an update to bug ' + bug_id + ' from board ' + board_id,
             MessageAttributes: {
                 boardId: {
                     DataType: 'Number',

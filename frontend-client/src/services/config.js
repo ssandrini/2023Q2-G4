d047940ea741626470e206a11a61123c9ca1ff7f
ssandrini <ssandrini@itba.edu.ar>
2023-12-20 00:20:47 -0300

    api url

--- frontend-client/src/services/config.js
+++ frontend-client/src/services/config.js
@@ -1,6 +1,6 @@
 // config.js
 import axios from 'axios';
-const API_URL = 'https://4kksw1g0ol.execute-api.us-east-1.amazonaws.com/dev';
+const API_URL = 'https://1cxxp3ngjc.execute-api.us-east-1.amazonaws.com/dev';
 
 const instance = axios.create({
   baseURL: API_URL, 

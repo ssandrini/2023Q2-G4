b720856f932ffce65d7152b92c6fda819a19f46e
gbeade <gbeade@itba.edu.ar>
2023-12-20 17:13:33 -0300

    Add modifications

--- frontend-client/src/services/config.js
+++ frontend-client/src/services/config.js
@@ -1,23 +1,23 @@
 // config.js
 import axios from 'axios';
-const API_URL = 'https://1cxxp3ngjc.execute-api.us-east-1.amazonaws.com/dev';
+const API_URL = 'https://pq7kwny0zg.execute-api.us-east-1.amazonaws.com/dev';
 
 const instance = axios.create({
   baseURL: API_URL, 
 });
 
-instance.interceptors.request.use(
-  (config) => {
-    const token = localStorage.getItem('token');
-    const access_token = localStorage.getItem('access_token');
-    if (access_token) {
-      config.headers.Authorization = `Bearer ${access_token}`;
-    }
-    return config;
-  },
-  (error) => {
-    return Promise.reject(error);
-  }
-);
+// instance.interceptors.request.use(
+//   (config) => {
+//     const token = localStorage.getItem('token');
+//     const access_token = localStorage.getItem('access_token');
+//     if (access_token) {
+//       config.headers.Authorization = `Bearer ${access_token}`;
+//     }
+//     return config;
+//   },
+//   (error) => {
+//     return Promise.reject(error);
+//   }
+// );
 
 export { instance };
--- frontend-client/src/views/LoginView/aws-exports.js
+++ frontend-client/src/views/LoginView/aws-exports.js
@@ -1,5 +1,5 @@
 export const awsExports = {
     "REGION" : "us-east-1",
-    "USER_POOL_ID": "us-east-1_Ol95yZjqs",
-    "USER_POOL_APP_CLIENT_ID": "3uvgfv3b34spd9ojo4hrm8ko6q"
+    "USER_POOL_ID": "us-east-1_1wYN5w60f",
+    "USER_POOL_APP_CLIENT_ID": "1sa921h5gn6jnv1ck7s6majhga"
 }
